MotorolaMobilityLLC/kernel-msm

drm/msm: free atomic state during lastclose
Ensure that the refcount of the atomic state is decremented
during lastclose. This will make sure that when the atomic
state is freed, all objects being held by it are released.

Change-Id: I547cf997b9eb80560c9646f688071fb6a41648b2
Signed-off-by: Fuad Hossain <fhossain@codeaurora.org>

drivers/gpu/drm/msm/msm_drv.c
@@ -979,9 +979,7 @@ static int msm_disable_all_modes(
 		drm_modeset_backoff(ctx);
 	}
 
-	/* on successful atomic commit state ownership transfers to framework */
-	if (ret != 0)
-		drm_atomic_state_put(state);
+	drm_atomic_state_put(state);
 
 	return ret;
 }

thanasxda/malakas_kernel_xiaomi_sdm845

Signed-off-by: Raphiel Rollerscaperers <rapherion@raphielgang.org>

drivers/gpu/drm/msm/msm_drv.c
@@ -907,9 +907,7 @@ static int msm_disable_all_modes(
 		drm_modeset_backoff(ctx);
 	}
 
-	/* on successful atomic commit state ownership transfers to framework */
-	if (ret != 0)
-		drm_atomic_state_free(state);
+	drm_atomic_state_free(state);
 
 	return ret;
 }